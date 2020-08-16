1ffcf880a122941de7f80c16f88ea152a9f9f46b
rajadev2019 <raja.dev2019@gmail.com>
2020-08-16 20:34:13 +0530

    Add project files.

---
+++ LiveXaml/LiveXaml/App.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace LiveXaml
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            // Initialize Live Reload.
+#if DEBUG
+            LiveReload.Init();
+#endif
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
